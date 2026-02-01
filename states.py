d6c0e2bbf8b15d78105fc7720a5bdfed8696b8c9
gerasimovee-hash <gerasimov.ee@gmail.com>
2026-02-01 14:57:13 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,1 +1,13 @@
+from aiogram.fsm.state import StatesGroup, State
+
+class HairTest(StatesGroup):
+    form = State()
+    thickness = State()
+    density = State()
+    scalp = State()
+    length = State()
+    porosity = State()
+    damage = State()
+    age = State()
+    result = State()
 
